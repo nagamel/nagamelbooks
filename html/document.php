0ece0bf9cf52b1e9c8c68214c890ef949604b6d1
karakitsune <arbeitfuchs@gmail.com>
2016-03-01 15:46:01 +0900

    160301
    160301update

---
+++ html/document.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="ja" class="nagamelbooks">
+<head>
+    <meta charset=utf-8>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <!--<link rel='stylesheet' id='habakiri-assets-css'  href='http://localhost/nagamel/nagamelbooks/wp-content/themes/habakiri/css/assets.min.css?ver=4.4.2' type='text/css' media='all' />-->
+    <link rel='stylesheet' id='habakiri-css'  href='http://localhost/nagamel/nagamelbooks/wp-content/themes/habakiri/style.min.css?ver=4.4.2' type='text/css' media='all' />
+    <link rel="stylesheet" href="../css/nagamelbooks.css" type='text/css' media='all' />
+    
+    <script type='text/javascript' src='http://localhost/nagamel/nagamelbooks/wp-includes/js/jquery/jquery.js?ver=1.11.3'></script>
+    <script type='text/javascript' src='http://localhost/nagamel/nagamelbooks/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.2.1'></script>
+    
+    <script type="text/javascript">
+        window._wpemojiSettings = {"baseUrl":"https:\/\/s.w.org\/images\/core\/emoji\/72x72\/","ext":".png","source":{"concatemoji":"http:\/\/localhost\/nagamel\/nagamelbooks\/wp-includes\/js\/wp-emoji-release.min.js?ver=4.4.2"}};
+        !function(a,b,c){function d(a){var c,d=b.createElement("canvas"),e=d.getContext&&d.getContext("2d"),f=String.fromCharCode;return e&&e.fillText?(e.textBaseline="top",e.font="600 32px Arial","flag"===a?(e.fillText(f(55356,56806,55356,56826),0,0),d.toDataURL().length>3e3):"diversity"===a?(e.fillText(f(55356,57221),0,0),c=e.getImageData(16,16,1,1).data.toString(),e.fillText(f(55356,57221,55356,57343),0,0),c!==e.getImageData(16,16,1,1).data.toString()):("simple"===a?e.fillText(f(55357,56835),0,0):e.fillText(f(55356,57135),0,0),0!==e.getImageData(16,16,1,1).data[0])):!1}function e(a){var c=b.createElement("script");c.src=a,c.type="text/javascript",b.getElementsByTagName("head")[0].appendChild(c)}var f,g;c.supports={simple:d("simple"),flag:d("flag"),unicode8:d("unicode8"),diversity:d("diversity")},c.DOMReady=!1,c.readyCallback=function(){c.DOMReady=!0},c.supports.simple&&c.supports.flag&&c.supports.unicode8&&c.supports.diversity||(g=function(){c.readyCallback()},b.addEventListener?(b.addEventListener("DOMContentLoaded",g,!1),a.addEventListener("load",g,!1)):(a.attachEvent("onload",g),b.attachEvent("onreadystatechange",function(){"complete"===b.readyState&&c.readyCallback()})),f=c.source||{},f.concatemoji?e(f.concatemoji):f.wpemoji&&f.twemoji&&(e(f.twemoji),e(f.wpemoji)))}(window,document,window._wpemojiSettings);
+    </script>
+    
+    <style type="text/css">.recentcomments a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+    <style>a{color:#337ab7}a:focus,a:active,a:hover{color:#23527c}.site-branding a{color:#000}.responsive-nav a{color:#000;font-size:12px}.responsive-nav a small{color:#777;font-size:10px}.responsive-nav a:hover small,.responsive-nav a:active small,.responsive-nav .current-menu-item small,.responsive-nav .current-menu-ancestor small,.responsive-nav .current-menu-parent small,.responsive-nav .current_page_item small,.responsive-nav .current_page_parent small{color:#777}.responsive-nav .menu>.menu-item>a,.header--transparency.header--fixed--is_scrolled .responsive-nav .menu>.menu-item>a{background-color:transparent;padding:23px 15px}.responsive-nav .menu>.menu-item>a:hover,.responsive-nav .menu>.menu-item>a:active,.responsive-nav .menu>.current-menu-item>a,.responsive-nav .menu>.current-menu-ancestor>a,.responsive-nav .menu>.current-menu-parent>a,.responsive-nav .menu>.current_page_item>a,.responsive-nav .menu>.current_page_parent>a,.header--transparency.header--fixed--is_scrolled .responsive-nav .menu>.menu-item>a:hover,.header--transparency.header--fixed--is_scrolled .responsive-nav .menu>.menu-item>a:active,.header--transparency.header--fixed--is_scrolled .responsive-nav .menu>.current-menu-item>a,.header--transparency.header--fixed--is_scrolled .responsive-nav .menu>.current-menu-ancestor>a,.header--transparency.header--fixed--is_scrolled .responsive-nav .menu>.current-menu-parent>a,.header--transparency.header--fixed--is_scrolled .responsive-nav .menu>.current_page_item>a,.header--transparency.header--fixed--is_scrolled .responsive-nav .menu>.current_page_parent>a{background-color:transparent;color:#337ab7}.responsive-nav .sub-menu a{background-color:#000;color:#777}.responsive-nav .sub-menu a:hover,.responsive-nav .sub-menu a:active,.responsive-nav .sub-menu .current-menu-item a,.responsive-nav .sub-menu .current-menu-ancestor a,.responsive-nav .sub-menu .current-menu-parent a,.responsive-nav .sub-menu .current_page_item a,.responsive-nav .sub-menu .current_page_parent a{background-color:#191919;color:#337ab7}.off-canvas-nav{font-size:12px}.responsive-nav,.header--transparency.header--fixed--is_scrolled .responsive-nav{background-color:transparent}#responsive-btn{background-color:transparent;border-color:#eee;color:#000}#responsive-btn:hover{background-color:#f5f5f5;border-color:#eee;color:#000}.habakiri-slider__transparent-layer{background-color:rgba( 0,0,0, 0.1 )}.page-header{background-color:#222;color:#fff}.pagination>li>a{color:#337ab7}.pagination>li>span{background-color:#337ab7;border-color:#337ab7}.pagination>li>a:focus,.pagination>li>a:hover,.pagination>li>span:focus,.pagination>li>span:hover{color:#23527c}.header{background-color:#fff}.header--transparency.header--fixed--is_scrolled{background-color:#fff !important}.footer{background-color:#111113}.footer-widget-area a{color:#777}.footer-widget-area,.footer-widget-area .widget_calendar #wp-calendar caption{color:#555}.footer-widget-area .widget_calendar #wp-calendar,.footer-widget-area .widget_calendar #wp-calendar *{border-color:#555}@media(min-width:992px){.responsive-nav{display:block}.off-canvas-nav,#responsive-btn{display:none !important}.header--2row{padding-bottom:0}.header--2row .header__col,.header--center .header__col{display:block}.header--2row .responsive-nav,.header--center .responsive-nav{margin-right:-1000px;margin-left:-1000px;padding-right:1000px;padding-left:1000px}.header--2row .site-branding,.header--center .site-branding{padding-bottom:0}.header--center .site-branding{text-align:center}}</style>
+</head>
+<body>
+    <div id="container">
+        <header id="header" class="header header--default ">
+            <div class="container">
+                <div class="row header__content">
+                    <div class="col-xs-10 col-md-4 header__col">
+                        <h1 id="logo" class="header-logo">
+                            <img src="http://localhost/nagamel/nagamelbooks/wp-content/themes/nagamelbooks/images/logo.svg" alt="">
+                        </h1>
+                        <!-- end .header__col --></div>
+                    <div class="col-xs-2 col-md-8 header__col global-nav-wrapper clearfix">
+
+                        <nav class="global-nav js-responsive-nav nav--hide" role="navigation">
+                            <div class="menu-glovalmenu-container"><ul id="menu-glovalmenu" class="menu"><li id="menu-item-7" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-7"><a href="http://localhost/nagamel/nagamelbooks/sample-page/">サンプルページ</a></li>
+                                </ul></div><!-- end .global-nav --></nav>
+                        <div id="responsive-btn"></div>
+                        <!-- end .header__col --></div>
+                    <!-- end .row --></div>
+                <!-- end .container --></div>
+            <!-- end #header --></header>
+    </div>
+    
+    <section>
+        <div class="container">
+            <h1>Titles</h1>
+            
+        </div>
+    </section>
+    
+    
+    
+    <script>
+        jQuery( function( $ ) {
+            $( '.js-responsive-nav' ).responsive_nav( {
+                direction: 'right'
+            } );
+        } );
+    </script>
+    <script type='text/javascript' src='http://localhost/nagamel/nagamelbooks/wp-content/themes/habakiri/js/app.min.js?ver=4.4.2'></script>
+</body>
+</html>
